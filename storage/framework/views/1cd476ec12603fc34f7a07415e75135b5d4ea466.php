c288478f3e513f6f984eedb97774d75922decc59
Kraschenko Alex <dev@disfood.ru>
2021-05-24 14:42:14 +0000

    fix

--- storage/framework/views/1cd476ec12603fc34f7a07415e75135b5d4ea466.php
+++
@@ -1,6 +0,0 @@
-<?php if($errors->any()): ?>
-    <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-        <div class="alert alert bg-red"><?php echo e($error); ?></div>
-    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
-<?php endif; ?>
-<?php /**PATH /var/www/html/disfood/resources/views/Admin/layouts/errors.blade.php ENDPATH**/ ?>

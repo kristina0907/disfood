f41e318d827561d0216a3f56c198f075e57beeb6
Aleksandr Krashenko <dev@disfood.ru>
2021-05-28 13:04:09 +0300

    add related products

--- resources/js/Pages/CatalogPage.vue
+++ resources/js/Pages/CatalogPage.vue
@@ -360,13 +360,13 @@
             </div>
             <div class="container_similar_products">
                 <div class="title_similar_products">Похожие предложения</div>
-                <div class="container_item_offer_catalog">
-                    <div class="item_product_category">
+                <div class="container_item_offer_catalog" v-if="relatedOffers.length">
+                    <div v-for="offer in relatedOffers" class="item_product_category">
                         <div class="info_item_offer_catalog">
                             <div class="image_info_item_offer_catalog"
                                  style="background-image: url(/images/offer.png);"></div>
                             <div>
-                                <div class="name_info_item_offer_catalog">Агро Альянс
+                                <div class="name_info_item_offer_catalog" v-if="offer.organization">{{offer.organization.name}}
                                     <span class="icon_star">
                                     <svg width="13" height="13" viewBox="0 0 13 13" fill="none"
                                          xmlns="http://www.w3.org/2000/svg">
@@ -401,135 +401,11 @@
                                 </div>
                             </div>
                             <div class="price_item_product_category">
-                                <div class="start_price_category_products">46, 34 ₽/ кг</div>
+                                <div class="start_price_category_products">{{offer.price}} ₽/ кг</div>
                             </div>
-                            <div class="make_deal">Подробнее</div>
-                            <div class="doc_item_offer">
-                                <button class="bay_item_offer_catalog" type="button" id="dropdownMenuButton"
-                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <svg width="25" height="24" viewBox="0 0 25 24" fill="none"
-                                         xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M1.5 2H3.89566C4.83305 2 5.64468 2.65106 5.84803 3.56614L6.5 6.5M6.5 6.5L7.60424 12.5733C8.12296 15.4263 10.6077 17.5 13.5075 17.5H16.2106C19.012 17.5 21.4406 15.5615 22.0614 12.8297L23.0835 8.33243C23.2969 7.39379 22.5834 6.5 21.6208 6.5H6.5Z"
-                                            stroke="#71BF45" stroke-width="2" stroke-linecap="round" />
-                                        <circle cx="9" cy="21" r="1" stroke="#71BF45" stroke-width="2" />
-                                        <circle cx="21" cy="21" r="1" stroke="#71BF45" stroke-width="2" />
-                                    </svg>
-                                </button>
-                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <a class="dropdown-item" href="#">Документ <span>PDF</span></a>
-                                    <a class="dropdown-item" href="#">Документ <span>PDF</span></a>
-                                    <a class="dropdown-item" href="#">Документ <span>PDF</span></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item_product_category">
-                        <div class="info_item_offer_catalog">
-                            <div class="image_info_item_offer_catalog"
-                                 style="background-image: url(/images/offer.png);"></div>
-                            <div>
-                                <div class="name_info_item_offer_catalog">Агро Альянс
-                                    <span class="icon_star">
-                                    <svg width="13" height="13" viewBox="0 0 13 13" fill="none"
-                                         xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M5.54894 0.927052C5.8483 0.0057416 7.1517 0.00574088 7.45106 0.927052L8.29611 3.52786C8.42999 3.93989 8.81394 4.21885 9.24717 4.21885H11.9818C12.9505 4.21885 13.3533 5.45846 12.5696 6.02786L10.3572 7.63525C10.0067 7.8899 9.86008 8.34127 9.99396 8.75329L10.839 11.3541C11.1384 12.2754 10.0839 13.0415 9.30017 12.4721L7.08779 10.8647C6.7373 10.6101 6.2627 10.6101 5.91222 10.8647L3.69983 12.4721C2.91612 13.0415 1.86164 12.2754 2.16099 11.3541L3.00604 8.75329C3.13992 8.34127 2.99326 7.8899 2.64277 7.63525L0.430391 6.02787C-0.353323 5.45846 0.0494523 4.21885 1.01818 4.21885H3.75283C4.18606 4.21885 4.57001 3.93989 4.70389 3.52786L5.54894 0.927052Z"
-                                            fill="#FFD789" />
-                                    </svg>
-
-                                </span>
-                                    <span class="text_star">4,5</span>
-                                </div>
-                                <div class="place_count_info_item_offer_catalog">
-                                    <span>50кг</span>
-                                    <span>25кг</span>
-                                    <span>10кг</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="container_item_offer_btn">
-                            <div class="delivery_item_similar_products">
-                                <div class="icon_delivery">
-                                    <svg width="25" height="24" viewBox="0 0 25 24" fill="none"
-                                         xmlns="http://www.w3.org/2000/svg">
-                                        <circle cx="12.5" cy="12" r="11" stroke="#71BF45" stroke-width="2" />
-                                        <path d="M12.5 7V13L15 15.5" stroke="#71BF45" stroke-width="2" stroke-linecap="round"
-                                              stroke-linejoin="round" />
-                                    </svg>
-                                </div>
-                                <div>
-                                    <div class="title_time_delivery">Доставка</div>
-                                    <div class="time_delivery">от 5 дней</div>
-                                </div>
-                            </div>
-                            <div class="price_item_product_category">
-                                <div class="start_price_category_products">46, 34 ₽/ кг</div>
-                            </div>
-                            <div class="make_deal">Подробнее</div>
-                            <div class="doc_item_offer">
-                                <button class="bay_item_offer_catalog" type="button" id="dropdownMenuButton"
-                                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                    <svg width="25" height="24" viewBox="0 0 25 24" fill="none"
-                                         xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M1.5 2H3.89566C4.83305 2 5.64468 2.65106 5.84803 3.56614L6.5 6.5M6.5 6.5L7.60424 12.5733C8.12296 15.4263 10.6077 17.5 13.5075 17.5H16.2106C19.012 17.5 21.4406 15.5615 22.0614 12.8297L23.0835 8.33243C23.2969 7.39379 22.5834 6.5 21.6208 6.5H6.5Z"
-                                            stroke="#71BF45" stroke-width="2" stroke-linecap="round" />
-                                        <circle cx="9" cy="21" r="1" stroke="#71BF45" stroke-width="2" />
-                                        <circle cx="21" cy="21" r="1" stroke="#71BF45" stroke-width="2" />
-                                    </svg>
-                                </button>
-                                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                                    <a class="dropdown-item" href="#">Документ <span>PDF</span></a>
-                                    <a class="dropdown-item" href="#">Документ <span>PDF</span></a>
-                                    <a class="dropdown-item" href="#">Документ <span>PDF</span></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="item_product_category">
-                        <div class="info_item_offer_catalog">
-                            <div class="image_info_item_offer_catalog"
-                                 style="background-image: url(/images/offer.png);"></div>
-                            <div>
-                                <div class="name_info_item_offer_catalog">Агро Альянс
-                                    <span class="icon_star">
-                                    <svg width="13" height="13" viewBox="0 0 13 13" fill="none"
-                                         xmlns="http://www.w3.org/2000/svg">
-                                        <path
-                                            d="M5.54894 0.927052C5.8483 0.0057416 7.1517 0.00574088 7.45106 0.927052L8.29611 3.52786C8.42999 3.93989 8.81394 4.21885 9.24717 4.21885H11.9818C12.9505 4.21885 13.3533 5.45846 12.5696 6.02786L10.3572 7.63525C10.0067 7.8899 9.86008 8.34127 9.99396 8.75329L10.839 11.3541C11.1384 12.2754 10.0839 13.0415 9.30017 12.4721L7.08779 10.8647C6.7373 10.6101 6.2627 10.6101 5.91222 10.8647L3.69983 12.4721C2.91612 13.0415 1.86164 12.2754 2.16099 11.3541L3.00604 8.75329C3.13992 8.34127 2.99326 7.8899 2.64277 7.63525L0.430391 6.02787C-0.353323 5.45846 0.0494523 4.21885 1.01818 4.21885H3.75283C4.18606 4.21885 4.57001 3.93989 4.70389 3.52786L5.54894 0.927052Z"
-                                            fill="#FFD789" />
-                                    </svg>
-
-                                </span>
-                                    <span class="text_star">4,5</span>
-                                </div>
-                                <div class="place_count_info_item_offer_catalog">
-                                    <span>50кг</span>
-                                    <span>25кг</span>
-                                    <span>10кг</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="container_item_offer_btn">
-                            <div class="delivery_item_similar_products">
-                                <div class="icon_delivery">
-                                    <svg width="25" height="24" viewBox="0 0 25 24" fill="none"
-                                         xmlns="http://www.w3.org/2000/svg">
-                                        <circle cx="12.5" cy="12" r="11" stroke="#71BF45" stroke-width="2" />
-                                        <path d="M12.5 7V13L15 15.5" stroke="#71BF45" stroke-width="2" stroke-linecap="round"
-                                              stroke-linejoin="round" />
-                                    </svg>
-                                </div>
-                                <div>
-                                    <div class="title_time_delivery">Доставка</div>
-                                    <div class="time_delivery">от 5 дней</div>
-                                </div>
-                            </div>
-                            <div class="price_item_product_category">
-                                <div class="start_price_category_products">46, 34 ₽/ кг</div>
-                            </div>
-                            <div class="make_deal">Подробнее</div>
+                            <a :href="'/catalog-page/' + offer.id">
+                                <div class="make_deal">Подробнее</div>
+                            </a>
                             <div class="doc_item_offer">
                                 <button class="bay_item_offer_catalog" type="button" id="dropdownMenuButton"
                                         data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -570,21 +446,31 @@ export default {
             countPackages:0,
             packages:[],
             filterPackages:[],
+            relatedOffers:[],
             volume:0,
             summ:0
         }
     },
     methods:{
-        getData()
+       async getData()
         {
             let self = this;
-            axios.get('/get/catalog/page/'+self.product_id).then((response) => {
+            await axios.get('/get/catalog/page/'+self.product_id).then((response) => {
                 if (response.data !== 'undefined' && response.data !== null) {
-                    this.product = response.data;
-                    this.filterPackages = response.data.category.packages;
+                    self.product = response.data;
+                    self.filterPackages = response.data.category.packages;
                 }
             })
+            await axios.get('/get/catalog/?category='+self.product.category_id+'&type='+ self.product.type_id)
+                    .then(response => {
+                        if (response.data !== 'undefined' && response.data !== null) {
+
+                            self.relatedOffers = response.data;
+                        }
+                    })
+
         },
+
         changeCount(val,pack)
         {
 

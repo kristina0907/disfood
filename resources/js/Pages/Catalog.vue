ddece7fc8296d4eadad82092aa451f046671dcf7
Aleksandr Krashenko <dev@disfood.ru>
2021-09-05 11:45:48 +0300

    fix front

--- resources/js/Pages/Catalog.vue
+++ resources/js/Pages/Catalog.vue
@@ -243,7 +243,7 @@
                                                     <router-link :to="{'name':'catalog-page',params:{id:product.id}}">
                                                         <div class="make_deal">Подробнее</div>
                                                     </router-link>
-                                                    <div class="doc_item_offer">
+                                                    <div class="doc_item_offer" v-if="product.documents">
                                                         <button class="bay_item_offer_catalog" type="button" id="dropdownMenuButton"
                                                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                                             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="19" viewBox="0 0 25 19" fill="none">

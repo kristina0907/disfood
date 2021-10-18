1eba0ec4618be888cb8cbdee8f0e8fcb3c377423
kristina <kris09.07@yandex.ru>
2021-10-18 11:20:38 +0300

    search

--- resources/js/Сomponents/SearchBar.vue
+++ resources/js/Сomponents/SearchBar.vue
@@ -3,20 +3,11 @@
         <input type="text" placeholder="Найти товар" v-model="searchBar" v-on:keyup="searchText">
         <div class="search-panel" v-if="Object.keys(searchResults).length">
             <div class="search-result-product" v-if="searchResults && searchResults.products">
-                <div class="col-md-4" style="margin-top: 20px;margin-bottom: 20px;">
-                    Продукты
-                </div>
-                <div class="col-md-12" v-for="product in searchResults.products">
-                    <div class="row product-item">
-                        <a href="#" @click="searchClickProduct(product.category_id,product.type_id)" class="col-md-12">
-                            <div class="row">
-                                <div class="col-md-1">
-                                    <img :src="product.image" alt="" class="search-img" >
-                                </div>
-                                <div class="col-md-11">
-                                    {{product.name}}
-                                </div>
-                            </div>
+                <div v-for="product in searchResults.products">
+                    <div class="search-result_product-item">
+                        <a href="#" @click="searchClickProduct(product.category_id,product.type_id)">
+                            <div class="title_search-result_product-item">{{product.name}}</div>
+                            <div class="price_search-result_product-item"><spa class="provider_search-result_product-item">36 поставщиков,</spa> от 75 ₽/ кг</div>
                         </a>
                     </div>
                 </div>
@@ -64,19 +55,41 @@
 .search-panel
 {
     display: block;
-   /* position: fixed;*/
-
+    position: absolute;
     float: left;
-    width:1100px;
-    height:400px;
-    top: 120px;
-    background: #fafafa;
+    width: 100%;
+    height: auto;
+    top: 46px;
+    background: #FFFFFF;
+    border: 1px solid #C5C5C5;
+    box-shadow: 0px 4px 20px rgba(34, 54, 74, 0.05);
+    border-radius: 10px;
+    padding: 15px 0;
 }
-.search-panel a
-{
-    color:#000;
-    cursor: pointer;
-
+.search-result_product-item{
+    padding: 10px 20px;
+}
+.search-result_product-item:hover{
+    background: #F0F2F4;
+}
+.search-result_product-item a{
+    font-weight: normal;
+    font-size: 15px;
+    line-height: 20px;
+    color: #22262A;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+}
+.search-result_product-item:hover a{
+    color: #71BF45;
+}
+.search-result_product-item:hover .provider_search-result_product-item{
+    color: #71BF45;
+}
+.provider_search-result_product-item{
+    color: #8898A8;
 }
 .product-item
 {

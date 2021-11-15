abb127249d208300099b5c481e792c3e918e1528
kristina <kris09.07@yandex.ru>
2021-11-15 15:28:32 +0300

    accriditation

--- resources/js/Pages/LK/Pages/Accreditation/AccreditationProvider.vue
+++ resources/js/Pages/LK/Pages/Accreditation/AccreditationProvider.vue
@@ -77,27 +77,27 @@
                                     <div class="col-xs-12 col-md-12 active_step">
                                         <div class="head_step_profile_accreditation_block">
                                             <div class="title_step">Отвечено</div>
-                                            <div class="number_step">{{step}}/5</div>
+                                            <div class="number_step">{{step}}/{{col}}</div>
                                         </div>
                                         <div class="container_profile_accreditation_block">
                                             <div class="row">
-                                                <div class="item_step_block active_half_step col">
+                                               <div class="item_step_block col" :class="{active_half_step : step == 1, completed_item_step: step <= col && step > 1 }">
+                                                <div class="half_step"></div>
+                                                <div class="half_step"></div>
+                                            </div>
+                                            <div class="item_step_block col" :class="{active_half_step : step == 2, completed_item_step: step <= col && step > 2}">
+                                                <div class="half_step"></div>
+                                                <div class="half_step"></div>
+                                            </div>
+                                            <div class="item_step_block col" :class="{active_half_step : step == 3, completed_item_step: step <= col && step > 3}">
+                                                <div class="half_step"></div>
+                                                <div class="half_step"></div>
+                                            </div>
+                                                <div class="item_step_block col" :class="{active_half_step : step == 4, completed_item_step: step <= col && step > 4}">
                                                     <div class="half_step"></div>
                                                     <div class="half_step"></div>
                                                 </div>
-                                                <div class="item_step_block col">
-                                                    <div class="half_step"></div>
-                                                    <div class="half_step"></div>
-                                                </div>
-                                                <div class="item_step_block col">
-                                                    <div class="half_step"></div>
-                                                    <div class="half_step"></div>
-                                                </div>
-                                                <div class="item_step_block col">
-                                                    <div class="half_step"></div>
-                                                    <div class="half_step"></div>
-                                                </div>
-                                                <div class="item_step_block col">
+                                                <div class="item_step_block col" :class="{active_half_step : step == 5}">
                                                     <div class="half_step"></div>
                                                     <div class="half_step"></div>
                                                 </div>
@@ -159,6 +159,93 @@
                                 </div>
                                 <div class="accreditation_question_item" v-if="step == 3">
                                     <div class="title_question_item">3. Вопрос номер шесть. Сколько процентов.</div>
+                                    <div class="row">
+                                        <div class="col-xs-12 col-md-12">
+                                            <div class="container_value_range">
+                                                <div>0%</div>
+                                                <div>100%</div>
+                                        </div>
+                                        <div>
+                                            <vue-range-slider class="custom_range" ref="slider" tooltip="false" v-model="value"></vue-range-slider>
+                                        </div>
+                                        <div class="value_question_range">
+                                            Вы готовы поделиться <span>{{value}} %</span>
+                                        </div>
+                                            </div>
+                                        </div>
+                            
+                                </div>
+                                 <div class="accreditation_question_item" v-if="step == 4">
+                                    <div class="title_question_item mr_0">4. Выберите категорию</div>
+                                    <div class="description_question_item">Для успешного прохождения аккредитации, необходимо выбрать категорию и параметр</div>
+                                    <div class="row">
+                                        <div class="col-xs-12 col-md-6">
+                                          <div class="select_container select_question_checkbox">
+                                            <div class="placeholder_select">Категория</div>
+                                            <multiselect 
+                                                    placeholder="Категория"
+                                                    selectLabel="Выберите категория"
+                                                    selectedLabel="Выбрано"
+                                                    deselectLabel="Нажмите еще раз чтобы удалить"
+                                                    v-model="valuesel" 
+                                                    :options="options"
+                                                    :multiple="true"
+                                                    :searchable="false"
+                                                    :allow-empty="false"
+                                                    track-by="value"
+                                                    :custom-label="customLabel"
+                                                    :close-on-select="false"
+                                                    @select=onSelect($event)
+                                                    @remove=onRemove($event)
+                                                    >
+                                                    <div class="select_question_checkbox_item" slot="option" slot-scope="scope" @click.self="select(scope.option)">
+                                                        <div class="checkbox accreditation_company_checkbox">
+                                                            <input type="checkbox" :id="scope.option.id" name="country" class="custom-checkbox" v-model="scope.option.checked" @focus.prevent>
+                                                            <label :for="scope.option.id">{{ scope.option.value }}</label>
+                                                        </div>
+                                                    </div>
+                                                </multiselect>
+                                            </div>
+                                        </div>
+                                         <div class="col-xs-12 col-md-6">
+                                            <div class="select_container select_question_checkbox">
+                                                <div class="placeholder_select">Параметр</div>
+                                                 <multiselect 
+                                                    placeholder="Параметр"
+                                                    selectLabel="Выберите параметр"
+                                                    selectedLabel="Выбрано"
+                                                    deselectLabel="Нажмите еще раз чтобы удалить"
+                                                    v-model="valuesel" 
+                                                    :options="options1"
+                                                    :multiple="true"
+                                                    :searchable="false"
+                                                    :allow-empty="false"
+                                                    track-by="value"
+                                                    :custom-label="customLabel"
+                                                    :close-on-select="false"
+                                                    @select=onSelect($event)
+                                                    @remove=onRemove($event)
+                                                    >
+                                                     <div class="select_question_checkbox_item" slot="option" slot-scope="scope" @click.self="select(scope.option)">
+                                                        <div class="checkbox accreditation_company_checkbox">
+                                                            <input type="checkbox" :id="scope.option.id" name="country" class="custom-checkbox" v-model="scope.option.checked" @focus.prevent>
+                                                            <label :for="scope.option.id">{{ scope.option.value }}</label>
+                                                        </div>
+                                                    </div>
+                                                </multiselect>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <button class="btn_transparent">
+                                        <span>
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <path d="M12 3V21" stroke="#71BF45" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                                <path d="M3 12L21 12" stroke="#71BF45" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                            </svg>
+                                        </span>
+                                        Добавить категорию
+                                    </button>
+                            
                                 </div>
                             </div>
                         </div>
@@ -178,19 +265,48 @@
 import UserLKHeader from "../../../../Сomponents/LK/UserLKHeader";
 import {mapState} from "vuex";
 import Leftsidebar from "../Settings/Leftsidebar";
+import 'vue-range-component/dist/vue-range-slider.css';
+import VueRangeSlider from 'vue-range-component';
+import Multiselect from 'vue-multiselect';
 export default {
     props: ['id'],
-    components: {Leftsidebar, UserLKHeader},
+    components: {Leftsidebar, UserLKHeader,VueRangeSlider,Multiselect},
     data(){
         return {
             loadingDoc:false,
-            step:1
+            step:1,
+            value: 50,
+            col:5,
+            valuesel: [],
+            options: [
+            { id:1, value: 'Категория А', checked: false },
+            { id:2, value: 'Категория В', checked: false },
+            { id:3, value: 'Категория С', checked: false }
+            ],
+            valuesel1: [],
+            options1: [
+            { id:4, value: 'Значение1', checked: false },
+            { id:5, value: 'Значение2', checked: false },
+            { id:6, value: 'Значение3', checked: false }
+            ]
         }
     },
     methods:{
         changeStep(){
             this.step ++;
-        }
+        },
+        customLabel (option) {
+      return `${option.value}`
+    },
+    onSelect (option) {
+      let index = this.options.findIndex(item => item.value==option.value);
+      this.options[index].checked = true;
+    },
+    
+    onRemove (option) {
+      let index = this.options.findIndex(item => item.value==option.value);
+      this.options[index].checked = false;
+    }
     },
     computed: {
     },

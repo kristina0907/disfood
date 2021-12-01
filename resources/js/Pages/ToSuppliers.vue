939138853ac3c9ffdf94444b11560110da5035c1
kristina <kris09.07@yandex.ru>
2021-12-01 14:55:57 +0300

    front

--- resources/js/Pages/ToSuppliers.vue
+++ resources/js/Pages/ToSuppliers.vue
@@ -3,15 +3,134 @@
         <div class="row">
             <HeaderCatalog></HeaderCatalog>
         </div>
-        <div class="content container">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Поставщикам</div>
-
-                    <div class="card-body">
-                        I'm an Поставщикам component.
+        <div class="container_main">
+            <div class="advantages_company_block">
+                   <div class="title_advantages_company_block">Преимущества</div>
+                   <div class="row">
+                       <div class="col-xs-12 col-md-3">
+                           <div class="item_advantages_company">
+                               <div class="img_item_advantages_company">
+                                   <svg width="88" height="81" viewBox="0 0 88 81" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                       <rect x="31" width="57" height="57" rx="28.5" fill="#CAE7BA" />
+                                       <path
+                                           d="M75.5 38.5H55.5V26H68.8074C69.3071 26 69.7953 26.1497 70.209 26.4298C70.6227 26.7099 70.943 27.1076 71.1286 27.5715L75.5 38.5Z"
+                                           stroke="#51668F" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" />
+                                       <path d="M5.5 46H55.5" stroke="#51668F" stroke-width="6" stroke-linecap="round"
+                                             stroke-linejoin="round" />
+                                       <path
+                                           d="M59.25 68.5C63.3921 68.5 66.75 65.1421 66.75 61C66.75 56.8579 63.3921 53.5 59.25 53.5C55.1079 53.5 51.75 56.8579 51.75 61C51.75 65.1421 55.1079 68.5 59.25 68.5Z"
+                                           stroke="#51668F" stroke-width="6" stroke-miterlimit="10" />
+                                       <path
+                                           d="M21.75 68.5C25.8921 68.5 29.25 65.1421 29.25 61C29.25 56.8579 25.8921 53.5 21.75 53.5C17.6079 53.5 14.25 56.8579 14.25 61C14.25 65.1421 17.6079 68.5 21.75 68.5Z"
+                                           stroke="#51668F" stroke-width="6" stroke-miterlimit="10" />
+                                       <path d="M51.75 61H29.25" stroke="#51668F" stroke-width="6" stroke-linecap="round"
+                                             stroke-linejoin="round" />
+                                       <path
+                                           d="M14.25 61H8C7.33696 61 6.70107 60.7366 6.23223 60.2678C5.76339 59.7989 5.5 59.163 5.5 58.5V23.5C5.5 22.837 5.76339 22.2011 6.23223 21.7322C6.70107 21.2634 7.33696 21 8 21H55.5V54.5047"
+                                           stroke="#51668F" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" />
+                                       <path
+                                           d="M55.5 54.5047V38.5H75.5V58.5C75.5 59.163 75.2366 59.7989 74.7678 60.2678C74.2989 60.7366 73.663 61 73 61H66.75"
+                                           stroke="#51668F" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" />
+                                   </svg>
+                               </div>
+                               <div class="text_item_advantages_company">Занимаемся логистикой<br />до вашего склада</div>
+                           </div>
+                       </div>
+                       <div class="col-xs-12 col-md-3">
+                           <div class="item_advantages_company">
+                               <div class="img_item_advantages_company">
+                                   <svg width="88" height="81" viewBox="0 0 88 81" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                       <rect x="31" width="57" height="57" rx="28.5" fill="#CAE7BA" />
+                                       <path
+                                           d="M68.0024 23.5H13.0024C11.6217 23.5 10.5024 24.6193 10.5024 26V66C10.5024 67.3807 11.6217 68.5 13.0024 68.5H68.0024C69.3832 68.5 70.5024 67.3807 70.5024 66V26C70.5024 24.6193 69.3832 23.5 68.0024 23.5Z"
+                                           stroke="#51668F" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" />
+                                       <path
+                                           d="M53 23.5V18.5C53 17.1739 52.4732 15.9021 51.5355 14.9645C50.5979 14.0268 49.3261 13.5 48 13.5H33C31.6739 13.5 30.4021 14.0268 29.4645 14.9645C28.5268 15.9021 28 17.1739 28 18.5V23.5"
+                                           stroke="#51668F" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" />
+                                       <path
+                                           d="M70.5026 40.4714C61.3847 45.7466 51.0339 48.5164 40.5 48.4998C29.968 48.5164 19.6188 45.7475 10.5021 40.4741"
+                                           stroke="#51668F" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" />
+                                       <path d="M36.75 38.5H44.25" stroke="#51668F" stroke-width="6" stroke-linecap="round"
+                                             stroke-linejoin="round" />
+                                   </svg>
+                               </div>
+                               <div class="text_item_advantages_company">Подготавливаем экспортную документацию</div>
+                           </div>
+                       </div>
+                       <div class="col-xs-12 col-md-3">
+                           <div class="item_advantages_company">
+                               <div class="img_item_advantages_company">
+                                   <svg width="88" height="81" viewBox="0 0 88 81" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                       <rect x="31" width="57" height="57" rx="28.5" fill="#CAE7BA" />
+                                       <path
+                                           d="M13 34.3333V16C13 15.337 13.2634 14.7011 13.7322 14.2322C14.2011 13.7634 14.837 13.5 15.5 13.5H65.5C66.163 13.5 66.7989 13.7634 67.2678 14.2322C67.7366 14.7011 68 15.337 68 16V34.3333C68 60.5881 45.7169 69.2865 41.2676 70.7616C40.7702 70.9328 40.2298 70.9328 39.7324 70.7616C35.2831 69.2865 13 60.5881 13 34.3333Z"
+                                           stroke="#51668F" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" />
+                                       <path d="M54.25 31L35.9166 48.5L26.75 39.75" stroke="#51668F" stroke-width="6"
+                                             stroke-linecap="round" stroke-linejoin="round" />
+                                   </svg>
+                               </div>
+                               <div class="text_item_advantages_company">Прогнозируем<br />цены на товары</div>
+                           </div>
+                       </div>
+                       <div class="col-xs-12 col-md-3">
+                           <div class="item_advantages_company">
+                               <div class="img_item_advantages_company">
+                                   <svg width="88" height="81" viewBox="0 0 88 81" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                       <rect x="31" width="57" height="57" rx="28.5" fill="#CAE7BA" />
+                                       <path d="M40.5 41V11" stroke="#51668F" stroke-width="6" stroke-linecap="round"
+                                             stroke-linejoin="round" />
+                                       <path d="M66.4807 26L14.5192 56" stroke="#51668F" stroke-width="6"
+                                             stroke-linecap="round" stroke-linejoin="round" />
+                                       <path
+                                           d="M40.5 11C45.7561 11.001 50.9197 12.3828 55.4736 15.0072C60.0276 17.6316 63.8122 21.4063 66.4484 25.9535C69.0845 30.5007 70.4798 35.6606 70.4944 40.9167C70.509 46.1727 69.1424 51.3403 66.5316 55.9021C63.9207 60.4638 60.1571 64.2595 55.6178 66.9092C51.0785 69.5588 45.9227 70.9693 40.6667 70.9995C35.4107 71.0297 30.2391 69.6784 25.6696 67.0811C21.1002 64.4838 17.2933 60.7315 14.6302 56.2"
+                                           stroke="#51668F" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" />
+                                       <path
+                                           d="M27.9982 34.1706L10.6663 44.177C10.5553 43.1216 10.4998 42.0611 10.5 40.9998C10.4991 35.2549 12.1475 29.6304 15.2493 24.7948C18.3511 19.9591 22.7759 16.1156 27.998 13.7209L27.9982 34.1706Z"
+                                           stroke="#51668F" stroke-width="6" stroke-linecap="round" stroke-linejoin="round" />
+                                   </svg>
+                               </div>
+                               <div class="text_item_advantages_company">Выступаем гарантом<br />для обеих сторон</div>
+                           </div>
+                       </div>
+                   </div>
+            </div>
+                <div class="consultation_manager_block">
+                <div class="title_advantages_company_block">Получите консультацию менеджера</div>
+                <form class="consultation_manager_form" action="">
+                    <div class="row">
+                    <div class="col-xs-12 col-md-3">
+                        <input type="text" placeholder="Имя">
+                    </div>
+                    <div class="col-xs-12 col-md-3">
+                        <input type="text" placeholder="Телефон">
+                    </div>
+                    <div class="col-xs-12 col-md-3">
+                        <div class="container_select_table_page">
+                            <div class="select_theme" v-on:click='showDropdown'>
+                                <span>{{ theme }}</span>
+                                <span>
+                                    <svg width="13" height="7" viewBox="0 0 13 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M1.66675 1L6.66675 6L11.6667 1" stroke="#22262A" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                    </svg>
+                                </span>
+                            </div>
+                            <div class="option_select_theme" v-if="isShowDropdown">
+                                <template>
+                                    <div v-for="item in themes" @click="changeCountPage(item)">{{ item }}</div>
+                                </template>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xs-12 col-md-3">
+                        <button>Заказать консультацию</button>
                     </div>
                 </div>
+                </form>
+                <div class="privacy_consultation_manager">Нажимая на кнопку “Заказать консультацию” Вы соглашаетесь с <a href="">Политикой конфиденциальности</a></div>
             </div>
         </div>
     </div>
@@ -23,6 +142,22 @@ export default {
     components:{
         HeaderCatalog
     },
+    data(){
+        return{
+            theme:"Тема обращения",
+            isShowDropdown: false,
+            themes:["Тема обращения 1","Тема обращения 2"],
+        }
+    },
+     methods:{
+         showDropdown() {
+            this.isShowDropdown=!this.isShowDropdown
+        },
+        changeCountPage(count){
+            this.theme = count;
+            this.isShowDropdown=!this.isShowDropdown;
+        }
+     },
     mounted() {
         console.log('Component mounted.')
     }
